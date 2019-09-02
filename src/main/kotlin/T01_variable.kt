a3357ade7d5b9b777bdf198baf1907323e6c60c2
Your Name <you@example.com>
2019-09-02 10:44:09 +0900

    first commit

---
+++ src/main/kotlin/T01_variable.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val data1 = "hello world"
+    var data2 = 100.0
+
+
+    data2 = 300.toDouble()
+    data2.toFloat()
+}
---
+++ src/main/kotlin/T02_fun.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val myVal = 100
+    val result = sum(myVal,20)
+    println(result)
+
+    val res2 = sum2(100,200)
+    println(res2)
+}
+
+fun sum(a:Int, b:Int=1000):Int{
+    var sum = 0
+    fun calSum(){
+        sum = a+b
+    }
+    calSum()
+    return sum
+}
+
+fun sum2(a:Int, b:Int) = a + b
